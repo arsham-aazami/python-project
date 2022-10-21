d706327cc2884e1f4fda1381ccd017a5b881a4c5
Arsham Aazami <arsham20302040aazami@gmail.com>
2022-10-21 14:52:21 +0330

    adding first exercise

---
+++ ex1/next_number_from_the_integer_passed.py
@@ -0,0 +1,8 @@
+
+def return_the_next_number_from_integer_passed(number):
+    number = number + 1
+    return number
+
+result1 = return_the_next_number_from_integer_passed(1)
+result2 = return_the_next_number_from_integer_passed(4)
+print(result1, result2)
