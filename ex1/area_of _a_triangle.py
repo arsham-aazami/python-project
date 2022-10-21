39428d5cf46a656ebaff0e8f70126dc9ce990b84
Arsham Aazami <arsham20302040aazami@gmail.com>
2022-10-21 14:57:23 +0330

    adding second exercise

---
+++ ex1/area_of _a_triangle.py
@@ -0,0 +1,8 @@
+def area_of_a_triangle(base,height):
+    area = (base * height) / 2
+    return area
+
+
+area1 = area_of_a_triangle(40, 12)
+area2 = area_of_a_triangle(32, 15)
+print(area1, area2)
