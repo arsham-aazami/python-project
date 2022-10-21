1963e35c4f1cdcd4ee51fd80b7e51c6412a3a198
Arsham Aazami <arsham20302040aazami@gmail.com>
2022-10-21 15:54:52 +0330

    debugging  day_of_weeks function and adding age_classifier function

--- ex1/2day_of_the_weeks.py
+++ ex1/2day_of_the_weeks.py
@@ -1,8 +1,8 @@
 def day_of_the_weeks(day):
-    user_input = input("Type a number between 1 and 7")
+
     # using dictionary to assign the integer number to the days of a week
     week_day={
-        1:"monday",
+        1: "monday",
         2: "tuesday",
         3: "wednesday",
         4: "thursday",
@@ -10,9 +10,9 @@ def day_of_the_weeks(day):
         6: "saturday",
         7: "sunday"
     }
-    return week_day[f"{user_input}"]
+    return week_day[user_input]
 
-
-day1 = day_of_the_weeks(1)
-day2 = day_of_the_weeks(5)
-print(day1, day2)
+#taking the user input
+user_input = int(input("Type a number between 1 and 7: "))
+specified_day = day_of_the_weeks(user_input)
+print(specified_day)
