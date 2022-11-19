b09fc5e774cd3e62f05eae94dbec82e5be028a98
Arsham Aazami <arsham20302040aazami@gmail.com>
2022-11-19 17:42:09 +0330

    adding help

--- ex3/face_interval.py
+++ ex3/face_interval.py
@@ -1,6 +1,19 @@
-def face_interval(list):
-    interval = max(list) - min(list)
-    for number in list:
+def face_interval(int_list):
+    """
+    ([int]) -> str
+    :param int_list: a list of integers
+    :return: ":)" if the interval is equal to any other number in the list otherwise returns ":("
+    """
+    interval = max(int_list) - min(int_list)
+    face = ""
+    for number in int_list:
         if number == interval:
-            return ":)"
-            continue
+            face = ":)"
+            break
+        else:
+            face = ":("
+            break
+    return face
+
+
+face_interval([2, 32, 1, 6])
