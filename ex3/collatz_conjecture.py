32c286b0ef10db814e939967ffece745131c80a2
Arsham Aazami <arsham20302040aazami@gmail.com>
2022-11-19 19:23:40 +0330

    updating collatz help

---
+++ ex3/collatz_conjecture.py
@@ -0,0 +1,21 @@
+def collatz_conjecture(number):
+	"""
+    (int) -> int
+    :param number:
+    :return: the number of steps that repeatedly performed
+    """
+	num_of_step = 0
+	if number > 0:
+		while number != 1:
+			if number % 2 == 0:
+				number /= 2
+			else:
+				number *= 3
+				number += 1
+			num_of_step += 1
+	else:
+		return "Oops! you should enter a positive number"
+	return num_of_step
+
+
+print(collatz_conjecture(10))
