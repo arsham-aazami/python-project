c22cdc252b38f79b6dc1476408d1209541acb4ce
Arsham Aazami <arsham20302040aazami@gmail.com>
2022-11-19 17:10:29 +0330

    commited

---
+++ ex3/multi_division.py
@@ -0,0 +1,16 @@
+def multi_division(a, b, c):
+    """
+        (int, int, int) -> boolean
+        :param a:
+        :param b:
+        :param c:
+        :return: A boolean if "a" time "b" is divisible by "c" function returns True otherwise returns False
+    """
+    a = a * b
+    if a % c == 0:
+        return True
+    else:
+        return False
+
+
+multi_division(42, 5, 10)
