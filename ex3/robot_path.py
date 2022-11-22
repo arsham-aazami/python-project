464a6eacac0746bcd90ab66d63a0c067c5890130
arsham-aazami <115664463+arsham-aazami@users.noreply.github.com>
2022-11-22 21:18:44 +0330

    Update robot_path.py

--- ex3/robot_path.py
+++ ex3/robot_path.py
@@ -1,32 +1,32 @@
-coordinate = [0, 0]
+def robot_path(destination_list):
+	num_of_n = 0
+	num_of_s = 0
+	num_of_e = 0
+	num_of_w = 0
 
-def distance(direction_list):
-    # implementing the location that robot can go (calculating the distance from the origin )
-    for direction in direction_list:
-        if direction == 'n':
-            coordinate[0] += 1
-        elif direction == 's':
-            coordinate[0] -= 1
-        elif direction == 'e':
-            coordinate[1] += 1
-        elif direction == 'w':
-            coordinate[1] -= 1
-    return coordinate
+	# The robot can only pass these two destinations
+	# ["e", "n", "e", "e", "n"],
+	# ["w", "n", "w", "n", "w", "w", "n"]
 
+    # we can explain that robot goes to the east for 3 times and north for 2 times in destination NO. 1
+	east_count_des1 = 3
+	north_count_des1 = 2
+	
+	# we can explain that robot goes to the north for three times and north for 4 times in destination NO. 2
+	north_count_des2 = 3
+	west_count_des2 = 4
 
-def robot_path(direction_list):
+	# obtaining the number of direction the robot pass to arrive the specified destination
+	for direction in destination_list:
+		if direction == "n": num_of_n += 1
+		elif direction == "s": num_of_s += 1
+		elif direction == "e": num_of_e += 1
+		elif direction == "w": num_of_w += 1
 
-    robot_default_dir = [
-            ["e", "n", "e", "e", "n"],
-            ["w", "n", "w", "n", "w", "w", "n"]
-    ]
-    dis_location1 = distance(robot_default_dir[0])
-    dis_location2 = distance(robot_default_dir[0])
-    dis_arbitrary_location = distance(direction_list)
-    if dis_arbitrary_location == dis_location1 or dis_arbitrary_location == dis_location2:
-        return True
-    else:
-        return False
+	if (num_of_n == north_count_des1 and num_of_e == east_count_des1) or (
+			num_of_w == west_count_des2 and num_of_n == north_count_des2):
+		return True
+	else:
+		return False
 
-
-robot_path(['e', 'e', 'n', 'w', 'n'])
+robot_path(["w", "n", "w", "n", "w", "w"]))
